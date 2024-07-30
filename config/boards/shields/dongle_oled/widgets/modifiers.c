db19cd8a79420088493da998da7662b683e594c3
Deozaan <Deozaan@users.noreply.github.com>
2024-07-30 04:39:45 -0600

    Don't use Mac keys

--- config/boards/shields/dongle_oled/widgets/modifiers.c
+++ config/boards/shields/dongle_oled/widgets/modifiers.c
@@ -70,8 +70,8 @@ struct modifier_symbol *modifier_symbols[] = {
     // this order determines the order of the symbols
     &ms_shift,
     &ms_control,
-    &ms_opt,
-    &ms_cmd,
+    &ms_win,
+    &ms_alt,
 };
 
 #define NUM_SYMBOLS (sizeof(modifier_symbols) / sizeof(struct modifier_symbol *))
