0226deb958a5218dc96ac0450758a6a791ee0dba
Deozaan <Deozaan@users.noreply.github.com>
2024-07-31 02:33:53 -0600

    Move some stuff around

--- config/boards/shields/dongle_oled/widgets/modifiers.c
+++ config/boards/shields/dongle_oled/widgets/modifiers.c
@@ -68,10 +68,10 @@ struct modifier_symbol ms_cmd = {
 
 struct modifier_symbol *modifier_symbols[] = {
     // this order determines the order of the symbols
+    &ms_win,
     &ms_shift,
     &ms_control,
     &ms_alt,
-    &ms_win,
 };
 
 #define NUM_SYMBOLS (sizeof(modifier_symbols) / sizeof(struct modifier_symbol *))
